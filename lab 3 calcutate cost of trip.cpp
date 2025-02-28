77b401c43b8b01e36e154b6487e89361271e11a8
Saqlain Mushtaq <saqlainjeegar@gmail.com>
2025-02-28 22:48:02 +0500

    Add files via upload

---
+++ lab 3 calcutate cost of trip.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+
+
+	//-_________________________-program no 5. -_________________________________-
+	
+	cout<<"Trip cost Estimater\n---------------"<<endl;
+	double d,fc,fe,tc;
+	cout<<"Enter the distance(km)          :";
+	cin>>d;
+	cout<<"Enter the fule cost(per liter)  :";
+	cin>>fc;
+	cout<<"Enter the fule efficiency       :";
+	cin>>fe;
+	tc=(d/fe)*fc;
+	cout<<"Total trip cost Rs              :"<<tc<<endl;
+	
+//	____________________________xxxx_________________________________
+
+	
+
+}
